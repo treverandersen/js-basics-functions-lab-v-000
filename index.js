91470e17ffb2f312def71cfceda730ab07f1e12a
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-22 23:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(d) {
+function distanceFromHqInBlocks() {
   let b = 42
   if (b > d) {
-    return b - d 
+    return b - d
   } else if (b < d) {
     return d - b
   }
+}
+
+function distanceFromHqInFeet(d) {
+  let b = 264
+  
 }
